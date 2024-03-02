7bf9bd3ce4fb33591071a12eda43a9099b5fac0c
Max Zinovik <zinovik@gmail.com>
2024-03-02 07:10:27 +0100

    add time to the request

--- src/get-page.ts
+++ src/get-page.ts
@@ -4,7 +4,9 @@ export const GAMES_PER_PAGE = 100;
 const URL = 'https://boardgamegeek.com/browse/boardgame/page/';
 
 export const getPage = async (pageNumber: number): Promise<string> => {
-    const { status, data: page } = await axios.get(`${URL}${pageNumber}`);
+    const { status, data: page } = await axios.get(
+        `${URL}${pageNumber}?${new Date().getTime()}`
+    );
     console.log(`Page ${pageNumber} | status: ${status}`);
 
     return page;
